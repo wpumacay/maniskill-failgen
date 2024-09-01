42607b519c74cde1efa8b8fd883c0fccbf90b842
wpumacay <wpumacay@gmail.com>
2024-08-31 20:17:32 -0500

    Adds modified record wrapper for sensor images

--- failgen/env_wrapper.py
+++ failgen/env_wrapper.py
@@ -6,13 +6,14 @@ import numpy as np
 
 from omegaconf import OmegaConf
 
-from mani_skill.utils.wrappers.record import RecordEpisode
-
-from failgen.task_solutions.soln_peg_insertion_side import solve as solvePegInsertionSide
+from failgen.task_solutions.soln_peg_insertion_side import (
+    solve as solvePegInsertionSide,
+)
 from failgen.task_solutions.soln_pick_cube import solve as solvePickCube
 from failgen.task_solutions.soln_plug_charger import solve as solvePlugCharger
 from failgen.task_solutions.soln_push_cube import solve as solvePushCube
 from failgen.task_solutions.soln_stack_cube import solve as solveStackCube
+from failgen.wrappers.record import RecordEpisode
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIGS_DIR = os.path.join(CURRENT_DIR, "configs")
@@ -28,14 +29,18 @@ MP_SOLUTIONS = {
 
 
 class FailgenWrapper:
-    def __init__(self, task_name: str, headless: bool, save_video: bool) -> None:
+    def __init__(
+        self, task_name: str, headless: bool, save_video: bool
+    ) -> None:
         self._task_name = task_name
         self._headless = headless
         self._save_video = save_video
         self._solve_fn = MP_SOLUTIONS[task_name]
         self._seed = 0
 
-        self._config = OmegaConf.load(os.path.join(CONFIGS_DIR, f"{task_name}.yaml"))
+        self._config = OmegaConf.load(
+            os.path.join(CONFIGS_DIR, f"{task_name}.yaml")
+        )
 
         self._env = gym.make(
             task_name,
@@ -43,7 +48,11 @@ class FailgenWrapper:
             control_mode="pd_joint_pos",
             render_mode=self._config.render_mode,
             reward_mode="dense",
-            sensor_configs=dict(shader_pack=self._config.shader),
+            sensor_configs=dict(
+                width=self._config.image_size[0],
+                height=self._config.image_size[1],
+                shader_pack=self._config.shader,
+            ),
             human_render_camera_configs=dict(shader_pack=self._config.shader),
             viewer_camera_configs=dict(shader_pack=self._config.shader),
             sim_backend=self._config.sim_backend,
@@ -55,6 +64,10 @@ class FailgenWrapper:
             save_video=self._save_video,
             video_fps=30,
             save_on_reset=False,
+            image_size=dict(
+                width=self._config.image_size[0],
+                height=self._config.image_size[1],
+            ),
         )
 
     def get_failure(self) -> bool:
@@ -68,5 +81,6 @@ class FailgenWrapper:
         self._seed += 1
         self._env.flush_trajectory()
         self._env.flush_video()
+        self._env.flush_video_multi()
 
         return success
